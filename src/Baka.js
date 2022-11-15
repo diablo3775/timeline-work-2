d5e23dd65bb97111f2a1cd94ad2cb0f7818be9d9
Anil Kumar <72590342+diablo3775@users.noreply.github.com>
2022-11-15 14:03:47 +0530

    Update Baka.js

--- src/Baka.js
+++ src/Baka.js
@@ -2,6 +2,7 @@ import { withStyles, makeStyles } from "@material-ui/core/styles";
 import React, { useEffect, useState } from 'react'
 import Tooltip from "@material-ui/core/Tooltip";
 import data from './a/name.json'
+import uuid4 from "uuid4";
 import './Baka.css'
 
 const HtmlTooltip = withStyles((theme) => ({
@@ -19,10 +20,10 @@ const Baka = () => {
   const [date, setDate] = useState("")
   const [CallerName, setCallerName] = useState("");
   const [submit, setSubmit] = useState(false)
-
-  console.log(data.filter((res) => res.result !== null))
-  // let filterdata = data.filter((res) => res.result !== null);
-
+  const [Ddisabled, setDDisabled] = useState(null)
+  // let [filterDropdown, setFilterDropDown] = useState([])
+  // let [uniqueCouse, setUniqueCouse] = useState([])
+  // let [uniqueName, setUniqueName] = useState([])
 
   const handleChangeCourse = (event) => {
     setDate(event.target.value);
@@ -43,7 +44,7 @@ const Baka = () => {
 
       // store the keys of the unique objects
       .map((e, i, final) => final.indexOf(e) === i && i)
-
+ 
       // eliminate the dead keys & store unique objects
       .filter((e) => arr[e])
 
@@ -54,26 +55,25 @@ const Baka = () => {
 
   useEffect(() => {
     const data = require("./a/name.json");
-    setData(data);
+    setData(data)
   }, []);
 
-  // let filterdata;
-  const uniqueCouse = getUnique(data, "Date");
-  const uniqueName = getUnique(data, "CallerName")
-  console.log("name ", uniqueName)
-  // filterdata = uniqueCouse.filter((res) => res.result !== null);
+  let uniqueCouse = getUnique(data, "Date");
+  let uniqueName = getUnique(data, "CallerName")
 
-  let filterdata;
-  useEffect(() => {
-    filterdata = uniqueName.filter((res) => res.result !== null);
-  }, [uniqueName, uniqueCouse])
-  // console.log(uniqueCouse.map(c => c.Date))
-  // console.log(uniqueName.map(n => n.CallerName))
+  let datajohn = data.filter((name) => name.CallerName === 'John Riley')
+  // console.log(datajohn)
 
-  const filterDropdown = data.filter(function (result) {
-    // if(result.result !== null) 
+  let datajenni = data.filter((name) => name.CallerName === "Jennifer.hart Hart")
+  // console.log(datajenni.map(a => a.CallerName))
+  let a = datajenni.map(a => a.CallerName);
+  console.log(a)
+
+  let filterDropdown = data.filter(function (result) {
     return result.Date === date && result.CallerName === CallerName;
   });
+
+  // console.log(filterDropdown)
 
   function formatPhoneNumber(phoneNumberString) {
     var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
@@ -84,6 +84,69 @@ const Baka = () => {
     }
     return null;
    }
+
+  //  useEffect(() => {
+  //   const elementss =  document.getElementById("bb");
+  //   if(filterDropdown.length > 0 ) {
+  //     console.log("data")
+  //   } else {
+  //     elementss[0].parentNode.removeChild(elementss[0])
+  //     console.log("no data")
+  //   }
+  //  },[date])
+
+  //  function removeElements(className) {
+  //   const elements =  document.getElementsByClassName(className);
+  //   if(filterDropdown.length > 0 ) {
+  //     console.log("data")
+  //   } else {
+  //     elements[0].parentNode.removeChild(elements[0])
+  //     console.log("no data")
+  //   }
+  //  }
+
+  //  removeElements(bb)
+//   function removeElementsByClass(className){
+//     const elements = document.getElementsByClassName(className);
+//     while(elements.length > 0){
+//         elements[0].parentNode.removeChild(elements[0]);
+//     }
+// }
+// removeElementsByClass("bb")
+// useEffect(() => {
+//   setBp(bp)
+let bp
+if(filterDropdown.length > 0) {
+    bp = "(Data)"
+  } else {
+    bp = "(No Data)"
+  }
+// }, [bp])
+
+let s = data.map((sa) => sa.Date)
+let asa = data.map((ba) => ba.PhoneNumber)
+
+// let e = {PhoneNumber: ""}
+
+
+// let am = data.some(ss => ss.PhoneNumber === ss.PhoneNumber)
+let am = data.map(ss => ss.PhoneNumber)
+
+// console.log(am)
+// console.log(am)
+// for(let i=0; i<=am.length;i++) {
+//   console.log(i)
+// }
+// if(typeof am !== "undefined") {
+//   console.log('ab')
+// } else {
+//   console.log('ba')
+// }
+// if(data.includes(asa)) {
+// console.log('saa')
+// } else {
+//   console.log('baa')
+// }
 
   return (
     <div className='timeline-container'>
@@ -104,8 +167,8 @@ const Baka = () => {
         <select className='dropdown' value={CallerName}  selected='' onChange={handleChangeName}>
           {uniqueName.map((course) => (
             <>
-            <option selected disabled hidden value=''>Select</option>
-            <option key={course.id} value={course.CallerName}>
+            <option disabled hidden value=''>Select</option>
+            <option key={uuid4()} value={course.CallerName}>
               {course.CallerName}
             </option>
             </>
@@ -114,24 +177,60 @@ const Baka = () => {
       </div>
       {filterDropdown.length > 0 ? <hr /> : ""}
       <div style={{ margin: '6px' }}>
-        <span>Date: </span>
+        {/* <span>Date: </span>
         <select className='dropdown date' value={date}  selected='' onChange={handleChangeCourse}>
           {uniqueCouse.sort((a, b) => a.Date > b.Date ? 1 : -1).map((course) => (
             <>
-            <option selected disabled hidden value=''>Select</option>
-            <option key={course.id} value={course.Date}>
-              {course.Date}
+            <option disabled hidden value=''>Select</option>
+            <option key={uuid4()} value={course.Date}>
+            {course.Date}
+            </option>
+            </>
+          ))}
+        </select> */}
+        {/* testing */}
+        {console.log(CallerName)}
+        {
+         CallerName === 'Jennifer.hart Hart' ?
+         <div>
+        <span>Date: </span>
+        <select className='dropdown date' value={date}  selected='' onChange={handleChangeCourse}>
+          {[...new Set(datajenni.map(item => item.Date))].sort((a, b) => a.Date > b.Date ? 1 : -1).map((course) => (
+            <>
+            <option disabled hidden value=''>Select</option>
+            <option key={uuid4()} value={course}>
+            {course}
             </option>
             </>
           ))}
         </select>
+        </div>
+        :
+        <div>
+        <span>Date: </span>
+        <select className='dropdown date' value={date}  selected='' onChange={handleChangeCourse}>
+          {[...new Set(datajohn.map(item => item.Date))].sort((a, b) => a.Date > b.Date ? 1 : -1).map((course) => (
+            <>
+            <option disabled hidden value=''>Select</option>
+            <option key={uuid4()} value={course}>
+            {course}
+            </option>
+            </>
+          ))}
+        </select>
+        </div>
+
+}
+        {/* testing */}
       </div>
       {/* <button className='generate' onClick={handleSubmit}>Generate</button> */}
 
       {/* {submit ? */}
         <div className='flex'>
-           {filterDropdown.length > 0 && "No Data" ? filterDropdown.sort((a, b) => Number(a.Time) > Number(b.Time) ? 1 : -1).map((course) => (
-            <div key={course.id} style={{ margin: "10px" }}>
+          {
+           filterDropdown.length > 0 && "No Data" ? 
+           filterDropdown.sort((a, b) => Number(a.Time) > Number(b.Time) ? 1 : -1).map((course) => (
+            <div key={uuid4()} style={{ margin: "10px" }}>
               <p className="time">{course.Time}</p>
               <HtmlTooltip
                 title={
@@ -141,6 +240,7 @@ const Baka = () => {
                     <span className="result">Message:</span>{(course.result
                       ? course.result.replace(/-->/g, 'to').replace(/[",']/g, '').slice(2, -3).split(/\r?\\n/).map(place => <div className='evenelement'><p className='place'> {place} </p></div>)
                       : "No Message")}
+                      {/* {console.log(course.result !== "" ? course.result :  console.log('okk'))} */}
                   </div>
                 }
                 interactive={true}
@@ -149,7 +249,9 @@ const Baka = () => {
               </HtmlTooltip>
               <div className="phoneNumber">{formatPhoneNumber(course.PhoneNumber)}</div>
             </div>
-          )) : <div className='nodata'>No Data</div>} 
+          )) 
+          : <div className='nodata'>No Data</div>
+          } 
         </div>
         {/* : null} */}
     </div>
